1e9617b387a5c39183e95bb2dc5cbbd9f482a441
youssef-mostafa1534 <youssefmostafa200895@gmail.com>
2026-08-09 09:53:26 +0300

    Trying out simple port scanning on single IP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::net::{IpAddr, SocketAddr, TcpStream};
+use std::str::FromStr;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    println!("=== NetScope: IP & Port Scanner ===");
+
+    let targetIpStr = "127.0.0.1";
+    let portsToScan = [21, 22, 80, 443, 3000, 8080];
+
+    let ip = match IpAddr::from_str(targetIpStr) {
+        Ok(parsedIp) => parsedIp,
+        Err(_) => {
+            eprintln!("Error: Invalid IP address string!");
+            return;
+        }
+    };
+
+    println!("Scanning target IP: {ip}\n");
+
+    for port in portsToScan {
+        scanPort(ip, port);
+    }
+}
+
+fn scanPort(ip: IpAddr, port: u16) {
+    let address = SocketAddr::new(ip, port);
+    let timeout = Duration::from_millis(500);
+
+    match TcpStream::connect_timeout(&address, timeout) {
+        Ok(_) => println!("[+] Port {:<5} : OPEN", port),
+        Err(_) => println!("[-] Port {:<5} : Closed / Filtered", port)
+    }
 }
